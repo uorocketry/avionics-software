101c67fa274b7ff59091f5cfa207443485be9293
jjaison <jaison.joe@orbcomm.com>
2025-08-06 19:42:26 -0400

    Impl: half and full bridge config

--- crates/straingauge_converter/src/lib.rs
+++ crates/straingauge_converter/src/lib.rs
@@ -8,15 +8,49 @@ const GAUGE_FACTOR: f64 = 2.0; // Gauge factor for the strain gauge
 const V_REF: f64 = 5.0; // Output voltage reference
 
 /*
-* Function to convert voltage to strain
+* Function to convert voltage to strain in quarter bridge configuration
+*
+* @param voltage: f64 - The voltage output from the strain gauge
+* @param gauge_factor: f64 - The gauge factor of the strain gauge default is 2.0
+* @return strain: f64 - The calculated strain value
+ */
+pub fn voltage_to_strain_quart(voltage: f64, gauge_factor: f64) -> f64 {
+    // Convert voltage to strain using the gauge factor and resistance
+    let mut strain = (voltage) / ((0.25) * GAUGE_FACTOR * V_REF);
+    // Apply correction factor
+    if gauge_factor != 0.0 || !gauge_factor.is_nan() {
+        strain = strain * (GAUGE_FACTOR / gauge_factor);
+    }
+    return strain;
+}
+
+/*
+* Function to convert voltage to strain in half bridge configuration
+*
+* @param voltage: f64 - The voltage output from the strain gauge
+* @param gauge_factor: f64 - The gauge factor of the strain gauge default is 2.0
+* @return strain: f64 - The calculated strain value
+ */
+pub fn voltage_to_strain_half(voltage: f64, gauge_factor: f64) -> f64 {
+    // Convert voltage to strain using the gauge factor and resistance
+    let mut strain = (voltage) / ((0.5) * GAUGE_FACTOR * V_REF);
+    // Apply correction factor
+    if gauge_factor != 0.0 || !gauge_factor.is_nan() {
+        strain = strain * (GAUGE_FACTOR / gauge_factor);
+    }
+    return strain;
+}
+
+/*
+* Function to convert voltage to strain in full bridge configuration
 *
 * @param voltage: f64 - The voltage output from the strain gauge
 * @param gauge_factor: f64 - The gauge factor of the strain gauge
 * @return strain: f64 - The calculated strain value
  */
-pub fn voltage_to_strain(voltage: f64, gauge_factor: f64) -> f64 {
+pub fn voltage_to_strain_full(voltage: f64, gauge_factor: f64) -> f64 {
     // Convert voltage to strain using the gauge factor and resistance
-    let mut strain = (voltage) / ((0.25) * GAUGE_FACTOR * V_REF);
+    let mut strain = (voltage) / (GAUGE_FACTOR * V_REF);
     // Apply correction factor
     if gauge_factor != 0.0 || !gauge_factor.is_nan() {
         strain = strain * (GAUGE_FACTOR / gauge_factor);
