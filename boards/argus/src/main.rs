5eb16994540239b5aa32b0b195f4c69929fdab1e
Darren Rahnemoon <darrenrahnemoon@gmail.com>
2025-09-14 18:40:55 -0400

    fix: embassy_stm32 feature mismatch

--- boards/argus/src/main.rs
+++ boards/argus/src/main.rs
@@ -14,18 +14,18 @@ mod utils;
 #[cfg(feature = "temperature")]
 mod temperature;
 
+use adc::service::AdcService;
+use defmt::info;
 use defmt_rtt as _;
+use embassy_executor::Spawner;
 use embassy_stm32::gpio::Pin;
 use panic_probe as _;
-use embassy_executor::Spawner;
-
-use utils::hal::configure_hal;
+use sd::service::SDCardService;
+use sd::task::{sd_card_task, test_sd_card};
 use static_cell::StaticCell;
+use utils::hal::configure_hal;
 use utils::types::AsyncMutex;
 
-use sd::service::SDCardService;
-use sd::task::sd_card_task;
-use adc::service::AdcService;
 use crate::adc::service::AdcConfig;
 
 // All services are singletons held in a static cell to initialize after peripherals are available
@@ -34,53 +34,17 @@ static SD_CARD_SERVICE: StaticCell<AsyncMutex<SDCardService>> = StaticCell::new(
 static ADC_SERVICE: StaticCell<AsyncMutex<AdcService>> = StaticCell::new();
 
 #[embassy_executor::main]
-async fn main(
-	spawner: Spawner,
-)
-{
+async fn main(spawner: Spawner) {
+	info!("Starting up...");
 	let peripherals = configure_hal();
 
-	let sd_card_service = SD_CARD_SERVICE.init(
-		AsyncMutex::new(
-				SDCardService::new(
-					peripherals.SPI1,
-					peripherals.PA5,
-					peripherals.PA7,
-					peripherals.PA6,
-					peripherals.PC4,
-				)
-		)
-	);
+	let sd_card_service = SD_CARD_SERVICE.init(AsyncMutex::new(SDCardService::new(peripherals.SPI1, peripherals.PA5, peripherals.PA7, peripherals.PA6, peripherals.PC4)));
 
-	let adc_service = ADC_SERVICE.init(
-		AsyncMutex::new(
-			AdcService::new(
-				peripherals.SPI4,
-				peripherals.PE2,
-				peripherals.PE6,
-				peripherals.PE5,
-				peripherals.DMA1_CH0,
-				peripherals.DMA1_CH1,
-				[
-					AdcConfig {
-						chip_select: peripherals.PE1.degrade(),
-						data_ready: peripherals.PB9.degrade(),
-						reset: peripherals.PE0.degrade(),
-						start: peripherals.PB0.degrade(),
-					},
-					AdcConfig {
-						chip_select: peripherals.PB8.degrade(),
-						data_ready: peripherals.PB6.degrade(),
-						reset: peripherals.PB7.degrade(),
-						start: peripherals.PB1.degrade(),
-					},
-				],
-			)
-		)
-	);
+	let adc_service = ADC_SERVICE.init(AsyncMutex::new(AdcService::new(peripherals.SPI4, peripherals.PE2, peripherals.PE6, peripherals.PE5, peripherals.DMA1_CH0, peripherals.DMA1_CH1, [AdcConfig { chip_select: peripherals.PE1.degrade(), data_ready: peripherals.PB9.degrade(), reset: peripherals.PE0.degrade(), start: peripherals.PB0.degrade() }, AdcConfig { chip_select: peripherals.PB8.degrade(), data_ready: peripherals.PB6.degrade(), reset: peripherals.PB7.degrade(), start: peripherals.PB1.degrade() }])));
 
 	#[cfg(feature = "temperature")]
-	spawner.must_spawn(crate::temperature::task::temperature_task(adc_service));
+	spawner.must_spawn(temperature::task::temperature_task(adc_service));
 
 	spawner.must_spawn(sd_card_task(sd_card_service));
+	spawner.must_spawn(test_sd_card());
 }
--- boards/argus/src/sd/task.rs
+++ boards/argus/src/sd/task.rs
@@ -1,10 +1,24 @@
+use core::fmt::Write;
+use core::str::FromStr;
+
+use heapless::String;
+
 use crate::sd::service::SDCardService;
+use crate::sd::types::OperationScope;
 use crate::utils::types::AsyncMutex;
 
 #[embassy_executor::task]
-pub async fn sd_card_task(
-	sd_card_service: &'static AsyncMutex<SDCardService>,
-)
-{
+pub async fn sd_card_task(sd_card_service: &'static AsyncMutex<SDCardService>) {
 	SDCardService::ensure_task(sd_card_service).await;
+}
+
+// SHOULD DO: move this to unit tests
+#[embassy_executor::task]
+pub async fn test_sd_card() {
+	for i in 0..5 {
+		let mut message: String<255> = String::new();
+		write!(message, "Hello, world {}! \n", i).unwrap();
+
+		SDCardService::enqueue_write(OperationScope::CurrentSession, String::from_str("test.txt").unwrap(), message).await;
+	}
 }
