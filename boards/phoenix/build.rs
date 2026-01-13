9ad14c5708485235a24e7dd9ca598d598037a2c4
Vortex148 <tubro2007@icloud.com>
2026-01-13 16:23:08 -0500

    fix: remove phoenix embedded_test usage

---
+++ boards/phoenix/build.rs
@@ -0,0 +1,7 @@
+// Uncomment if embedded test is being used
+// fn main() {
+// 	// Needed by embedded_test crate to function
+// 	println!("cargo::rustc-link-arg=-Tembedded-test.x");
+// }
+
+fn main() {}
